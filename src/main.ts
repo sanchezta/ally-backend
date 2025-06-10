46c2dd00ffc832b80945b382d105d0dfe95f473d
Adrian Sanchez <adriansanchezta62@gmail.com>
2025-06-10 07:35:03 -0400

    feat: connection mysql

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   await app.listen(envs.port);
-  console.log(`Application is running on port ${envs.port}`);
+  logger.log(`Application is running on port ${envs.port}`);
 }
 bootstrap();
